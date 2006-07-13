32b2eab4c3b08b1b8617d5fa8df9d32d7c2d6a7e
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2006-07-13 03:02:30 +0000

    add example of users.js
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@73 8f797353-fa07-0410-9347-d4c221381513

---
+++ examples/user.js
@@ -0,0 +1,25 @@
+
+# disable dialog for HTTPS pages
+user_pref("security.warn_entering_secure", false);
+user_pref("security.warn_entering_weak", false);
+user_pref("security.warn_leaving_secure", false);
+user_pref("security.warn_viewing_mixed", false)
+
+# Tune for Japanese
+user_pref("intl.accept_languages", "ja, en-us, en");
+user_pref("intl.charset.default", "Shift_JIS");
+user_pref("intl.charset.detector", "ja_parallel_state_machine");
+
+# disable javascript...
+user_pref("javascript.enabled", false);
+
+# speed tune...
+user_pref("nglayout.initialpaint.delay", 100);
+user_pref("network.http.max-connections", 45);
+user_pref("network.http.pipelining", true);
+user_pref("network.http.pipelining.firstrequest", true);
+user_pref("network.http.pipelining.maxrequests", 8);
+
+# save mem
+user_pref("browser.sessionhistory.max_total_viewers", 0)
+
