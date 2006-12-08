1e279291f2c0db962bff003adcb24d1df80430ec
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2006-12-08 05:22:37 +0000

    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@114 8f797353-fa07-0410-9347-d4c221381513

---
+++ examples/stat-ts.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+require 'drb'
+#require 'rinda/tuplespace'
+require 'pp'
+
+DRb.start_service('drbunix:')
+ts = DRbObject.new_with_uri(ARGV[0])
+
+loop {
+  r = ts.read_all([nil, nil, nil, nil])
+  stat = Hash.new(0)
+  r.each {|i| stat[i[2]] += 1 }
+  puts "#{Time.now.strftime('%Y-%m-%d %T')}: #{stat.map{|v| "%s: %2d" % v }.join(', ')}, total: #{r.length} tabpples."
+  sleep 10
+}
+
