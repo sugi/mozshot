96b93f820b56de11012b2f3a16fb9a9e13c77351
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2006-12-27 06:46:32 +0000

    add arguments to specify sleep sec
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@117 8f797353-fa07-0410-9347-d4c221381513

--- examples/dump-ts.rb
+++ examples/dump-ts.rb
@@ -6,12 +6,13 @@ require 'pp'
 
 DRb.start_service('drbunix:')
 ts = DRbObject.new_with_uri(ARGV[0])
+sleep_sec = ARGV[1] ? ARGV[1].to_i : 10
 
 loop {
   r = ts.read_all([nil, nil, nil, nil])
   r.each {|i| pp i[0..2] }
   puts "#{r.length} tabpples."
-  sleep 3
+  sleep sleep_sec
   puts "-----------"
 }
 
