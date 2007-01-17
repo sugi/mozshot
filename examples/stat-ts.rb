74c147de230e5014a55126da2f4dc9086d99265b
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2007-01-17 11:09:37 +0000

    sorted output for type.
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@121 8f797353-fa07-0410-9347-d4c221381513

--- examples/stat-ts.rb
+++ examples/stat-ts.rb
@@ -11,7 +11,7 @@ loop {
   r = ts.read_all([nil, nil, nil, nil])
   stat = Hash.new(0)
   r.each {|i| stat[i[2]] += 1 }
-  puts "#{Time.now.strftime('%Y-%m-%d %T')}: #{stat.map{|v| "%s: %2d" % v }.join(', ')}, total: #{r.length} tabpples."
+  puts "#{Time.now.strftime('%Y-%m-%d %T')}: #{stat.sort_by{|k,| k.to_s}.map{|v| "%s: %2d" % v }.join(', ')}, total: #{r.length} tabpples."
   sleep 10
 }
 
