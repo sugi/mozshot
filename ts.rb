6c1ea0cd72ff07f4e7f96afceb18071968bc2579
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2007-04-05 16:52:48 +0000

    try to force GC...
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@137 8f797353-fa07-0410-9347-d4c221381513

--- ts.rb
+++ ts.rb
@@ -7,4 +7,5 @@ ts = Rinda::TupleSpace.new
 DRb.start_service('drbunix:/home/sugi/.mozilla/mozshot/drbsock',
 		  ts, {:UNIXFileMode => 0600})
 puts DRb.uri
+Thread.new { loop { sleep 600; GC.start } }
 DRb.thread.join
