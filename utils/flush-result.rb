359f4d3f34682e6af9fa9544ad74076a85f223ae
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2007-07-18 08:38:56 +0000

    check single color, more.
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@149 8f797353-fa07-0410-9347-d4c221381513

--- utils/flush-result.rb
+++ utils/flush-result.rb
@@ -4,11 +4,13 @@ require 'drb'
 require 'rinda/rinda'
 require 'pstore'
 require 'pp'
+require 'digest/md5'
 load 'shot.cgi'
 
 DRb.start_service('drbunix:')
 ts = DRbObject.new_with_uri(ARGV[0])
 
+write_count = 0
 r = ts.read_all([:ret, nil, nil, nil])
 r.each {|i|
   cur = nil
@@ -24,13 +26,30 @@ r.each {|i|
       'Timestamp'   => ret[:timestamp].to_i,
       'OriginalURI' => ret[:req][:uri]
   }
-  image = Magick::Image.from_blob(ret[:image])[0]
-  if image.number_colors == 1
-    ret.delete(:image)
-    msg = "drop(single color): #{ret.inspect}"
-    STDERR.puts "drop(single color): #{ret.inspect}"
-    next
+
+  if Magick::Image.from_blob(ret[:image])[0].number_colors == 1
+    ret.delete :image
+    puts "drop(single color): #{ret.inspect}"
+    if File.exists? shot.cache_path
+      puts "snapshot file already exists, ignore."
+      next
+    else
+      badfile = shot.cache_path+".badmark"
+      i = 0
+      open(badfile, "a+") { |b|
+	i = b.read.to_i
+	b.rewind
+	b << i+1
+      }
+      puts "set bad count = #{i}"
+      i < 3 and next
+      File.delete badfile
+      msg = "bad cound limit exceeded. writing force: #{ret[:req][:uri]}"
+      puts msg
+      $stderr.puts msg
+    end
   end
+
   image = shot.add_metadata(ret[:image], metadata)
   if ret[:req] && ret[:req][:opt][:effect]
     image = shot.do_effect(image)
@@ -41,8 +60,10 @@ r.each {|i|
   begin
     File.rename(shot.cache_path+".tmp", shot.cache_path)
     puts "write: #{shot.cache_path} (#{ret[:req][:uri]})"
+    write_count += 1
   end
 }
+puts "complete: wrote out #{write_count} images."
 
 # vim: set sw=2:
 # vim: set sts=2:
