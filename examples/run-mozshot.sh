1e2379d396da5bbc9fd194d737808e551ae864a9
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2007-07-25 18:38:19 +0000

    add daemon runner.
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@155 8f797353-fa07-0410-9347-d4c221381513

---
+++ examples/run-mozshot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if test -z "$DISPLAY"; then
+  echo "You need to set DISPLAY env" >&2
+  exit 1
+elif [ "$DISPLAY" = ":0.0" ]; then
+  echo "You really want to run uner DISPLAY :0.0?" >&2
+  exit 1
+fi
+
+export TEMP=/tmp/mozshot
+export TMP=/tmp/mozshot
+export MOZSHOT_DAEMON_SOCK=druby://desire.arege.jp:7524
+export MOZILLA_FIVE_HOME=/usr/lib/xulrunner
+unset XAUTHORITY
+unset GNOME_KEYRING_SOCKET
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+unset SSH_AUTH_SOCK
+unset GNOME_KEYRING_SOCKET
+unset GPG_AGENT_INFO
+unset GTK_IM_MODULE
+unset XMODIFIERS
+
+if test -z "$1"; then
+  id=${DISPLAY#:}
+else
+  id=$1
+fi
+
+cd `dirname $0`/.. || exit 1
+while :; do
+  ruby -r timestamp.rb mozshot.rb >> ~/data/log/mozshot-${id}.log 2>&1
+  sleep 0.3
+done
