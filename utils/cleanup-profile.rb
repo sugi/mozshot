458e9d5e044dc7658fded076a7cffc55af52c8c3
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2007-07-29 02:26:25 +0000

    parallel check for each profile dirs.
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@160 8f797353-fa07-0410-9347-d4c221381513

--- utils/cleanup-profile.rb
+++ utils/cleanup-profile.rb
@@ -6,44 +6,49 @@ require 'fileutils'
 require 'drb'
 require 'timeout'
 
+threads = []
+
 ARGV.each { |p|
-  #File.directory? "#{p}/Cache" or next
-  begin
-    UNIXSocket.open("#{p}/drbsock").close
-    puts "ok: #{p}"
+  threads << Thread.new {
+    #File.directory? "#{p}/Cache" or next
     begin
-      drb = DRbObject.new_with_uri("drbunix:#{p}/drbsock")
+      UNIXSocket.open("#{p}/drbsock").close
+      puts "connect ok: #{p}"
       begin
-        timeout(30) {
-	  puts drb.inspect
-	  puts drb.to_s
-	  puts drb.ping.inspect
-        }
-      rescue Timeout::Error
-	puts "killing: #{p}"
+	drb = DRbObject.new_with_uri("drbunix:#{p}/drbsock")
 	begin
-	  Process.kill(:SEGV, File.basename(p).sub(/^proc-/, '').to_i)
-	  sleep 1
-	  Process.kill(:KILL, File.basename(p).sub(/^proc-/, '').to_i)
-	rescue Errno::ESRCH
-	  # ignore
+	  timeout(30) {
+	    drb.ping
+	  }
+          puts "ping ok: #{p}"
+	rescue Timeout::Error
+	  puts "killing: #{p}"
+	  begin
+	    Process.kill(:SEGV, File.basename(p).sub(/^proc-/, '').to_i)
+	    sleep 1
+	    Process.kill(:KILL, File.basename(p).sub(/^proc-/, '').to_i)
+	  rescue Errno::ESRCH
+	    # ignore
+	  end
+	  FileUtils.rm_rf(p)
+	rescue NoMethodError => e
+	  STDERR.puts "wierd profile: #{p}; #{e.inspect}"
+	  puts "wierd profile: #{p}; #{e.inspect}"
 	end
-	FileUtils.rm_rf(p)
-      rescue NoMethodError => e
-	STDERR.puts "wierd profile: #{p}; #{e.inspect}"
-	puts "wierd profile: #{p}; #{e.inspect}"
+      end
+    rescue Errno::ECONNREFUSED, Errno::ENOENT, DRb::DRbConnError
+      FileUtils.rm_rf(p)
+      puts "cleanup: #{p}"
+      begin
+	Process.kill(:KILL, File.basename(p).sub(/^proc-/, '').to_i)
+      rescue Errno::ESRCH
+	# ignore
       end
     end
-  rescue Errno::ECONNREFUSED, Errno::ENOENT, DRb::DRbConnError
-    FileUtils.rm_rf(p)
-    puts "cleanup: #{p}"
-    begin
-      Process.kill(:KILL, File.basename(p).sub(/^proc-/, '').to_i)
-    rescue Errno::ESRCH
-      # ignore
-    end
-  end
+  }
 }
+
+threads.each {|t| t.join }
 
 # vim: set sw=2:
 # vim: set sts=2:
