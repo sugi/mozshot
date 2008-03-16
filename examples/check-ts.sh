c429f51a164fbd2830d282128b0bc389a2adf495
sugi <sugi@8f797353-fa07-0410-9347-d4c221381513>
2008-03-16 23:28:52 +0000

    add tupple space checker...
    git-svn-id: svn+ssh://svn.nemui.org/home/sugi/data/svnroot/mozshot/trunk@182 8f797353-fa07-0410-9347-d4c221381513

---
+++ examples/check-ts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#pid=`pidof -x ts.rb`
+pid=`< /home/sugi/www/mozshot/ts.pid`
+restart=false
+
+if ! timeout 2 ruby ~/www/mozshot/utils/ping-ts.rb druby://:7524; then
+  echo "ts.rb not respond ping, restarting..."
+  restart=true
+fi
+
+if test -z "$pid"; then
+  echo "ts.rb not found"
+  exit 1
+fi
+
+size=`cut -d' ' -f1 /proc/$pid/statm`
+size=$((size*4/1024))
+
+if [ "$size" -gt 300 ]; then
+  echo "ts.rb memory size is over 300M, restarting..."
+  ps axuw | egrep '[t]s.rb'
+  restart=true
+fi
+
+if [ "$restart" = "true" ]; then
+  kill -HUP $pid
+  sleep 1
+  kill -KILL $pid > /dev/null 2>&1
+fi
